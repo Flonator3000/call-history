c6c7788f2592f453e66029d1ef84d4927fdc9974
Flonator3000 <florian.woerle@yahoo.de>
2022-12-17 18:41:20 +0100

    Use MediaQueryUtil in CallLogListRow

--- lib/widget/call_log_list_row.dart
+++ lib/widget/call_log_list_row.dart
@@ -1,3 +1,4 @@
+import 'package:call_history/widget/media_query_util.dart';
 import 'package:call_log/call_log.dart';
 import 'package:flutter/material.dart';
 
@@ -11,19 +12,21 @@ class CallLogListRow extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final MediaQueryUtil mediaQueryUtil = MediaQueryUtil.of(context);
+
     return Container(
-      margin: const EdgeInsets.only(bottom: 7.0),
-      height: 100,
-      decoration: const BoxDecoration(
-        color: Color.fromRGBO(224, 224, 224, 1),
+      margin: EdgeInsets.only(bottom: mediaQueryUtil.height(0.01)),
+      height: mediaQueryUtil.height(0.13),
+      decoration: BoxDecoration(
+        color: const Color.fromRGBO(224, 224, 224, 1),
         border: Border(
           bottom: BorderSide(
-            width: 0.5,
+            width: mediaQueryUtil.height(0.001),
           ),
         ),
       ),
       child: Padding(
-        padding: const EdgeInsets.all(20.0),
+        padding: EdgeInsets.all(mediaQueryUtil.height(0.025)),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
@@ -31,12 +34,10 @@ class CallLogListRow extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
-                  callLogEntry.name != null ? callLogEntry.name! : "Unbekannt",
+                  callLogEntry.name != null ? callLogEntry.name! : "Unknown",
                   style: const TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
                 ),
-                const SizedBox(
-                  height: 5.0,
-                ),
+                SizedBox(height: mediaQueryUtil.height(0.01)),
                 Text(
                   callLogEntry.formattedNumber != null ? callLogEntry.formattedNumber! : callLogEntry.number!,
                   style: const TextStyle(color: Colors.black),
