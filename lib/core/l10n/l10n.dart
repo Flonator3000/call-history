498e636fce799e5722d2b946b4da363b77018b00
Flonator3000 <florian.woerle@yahoo.de>
2022-12-21 18:45:59 +0100

    Add other locales

--- lib/core/l10n/l10n.dart
+++ lib/core/l10n/l10n.dart
@@ -4,7 +4,12 @@ import 'dart:ui';
 class L10n {
   static final all = [
     const Locale(
-      'en',
-    )
+      'en'
+    ),
+    const Locale('de'),
+    const Locale('el'),
+    const Locale('ar'),
+    const Locale('fr'),
+    const Locale('es'),
   ];
 }
